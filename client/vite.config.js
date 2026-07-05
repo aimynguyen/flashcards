e3a3f414d448c7e2d9005f391b066e7d35f6c45f
my <24521092@gm.uit.edu.vn>
2026-07-05 22:32:57 +0700

    Add GitHub Pages deployment config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [react()],
+  base: isProduction ? "/flashcards/" : "/",
   envPrefix: ["VITE_", "NEXT_PUBLIC_"],
   server: {
     host: "0.0.0.0",
